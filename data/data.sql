480d23f2fa09ec06877576f79874621aff69a4ce
AlainTRECHE <treche.alain@gmail.com>
2022-06-01 18:03:10 +0200

    edit data files

--- data/data.sql
+++ data/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO "question" ("content") VALUES 
+INSERT INTO "question" ("content","answer_id") VALUES 
 ('Comment le père Noël entrera-t-il chez vous pour vous apporter vos cadeaux ?'),
 ('Comment s"appelle l"arbre de Noël ?'),
 ('Avec quoi va-t-on décorer cet arbre de Noël ?'),
@@ -12,16 +12,16 @@ INSERT INTO "question" ("content") VALUES
 ('Dans quoi le père Noël met-il les cadeaux ?'),
 ('Où le père Noël habite-t-il ?');
 
-INSERT INTO "answer" ("content") VALUES
-('Par la porte avec ses clefs magiques'),('Par la fenêtre'),('Par la cheminée'),
-('Le sapin'),('Le palmier'),('Le chêne'),
-('Des lampes de Noël'),('Des jouets de Noël'),('Des guirlandes de Noël'),
-('Un bisou de la mère Noël'),('Un bon pour ranger votre chambre'),('Des cadeaux'),
-('En vert et blanc'),('En rouge et blanc'),('En jaune et blanc'),
-('Avec son hélicoptère'),('Avec son char'),('Avec son traîneau'),
-('Des cerfs'),('Des rennes'),('Des chevaux'),
-('Une bûche'),('Une forêt noire'),('Une tarte aux pommes'),
-('Les lutins'),('Les enfants'),('Les nains'),
-('Pour fêter la mort de Jésus'),('Pour fêter la naissance de Dieu'),('Pour fêter la naissance de Jésus'),
-('Dans sa hotte'),('Dans son sac à dos'),('Dans son bonnet'),
-('Au Canada'),('Au pôle Sud'),('Au pôle Nord');
+INSERT INTO "answer" ("content","question_id") VALUES
+('Par la porte avec ses clefs magiques',1),('Par la fenêtre',1),('Par la cheminée',1),
+('Le sapin',2),('Le palmier',2),('Le chêne',2),
+('Des lampes de Noël',3),('Des jouets de Noël',3),('Des guirlandes de Noël',3),
+('Un bisou de la mère Noël',4),('Un bon pour ranger votre chambre',4),('Des cadeaux',4),
+('En vert et blanc',5),('En rouge et blanc',5),('En jaune et blanc',5),
+('Avec son hélicoptère',6),('Avec son char',6),('Avec son traîneau',6),
+('Des cerfs',7),('Des rennes',7),('Des chevaux',7),
+('Une bûche',8),('Une forêt noire',8),('Une tarte aux pommes',8),
+('Les lutins',9),('Les enfants',9),('Les nains',9),
+('Pour fêter la mort de Jésus',10),('Pour fêter la naissance de Dieu',10),('Pour fêter la naissance de Jésus',10),
+('Dans sa hotte',11),('Dans son sac à dos',11),('Dans son bonnet',11),
+('Au Canada',12),('Au pôle Sud',12),('Au pôle Nord',12);
--- data/table.sql
+++ data/table.sql
@@ -1,13 +1,24 @@
-CREATE TABLE "question" (
+BEGIN.
+
+DROP TABLE IF EXISTS "answer","question";
+
+CREATE TABLE IF NOT EXISTS "answer" (
     "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "content" TEXT NOT NULL,
+    "question_id" INTEGER NOT NULL,
     "created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" timestamptz
 );
 
-CREATE TABLE "answer" (
+CREATE TABLE IF NOT EXISTS "question" (
     "id" INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     "content" TEXT NOT NULL,
+    "answer_id" INTEGER NOT NULL REFERENCES "answer" ("id"),
     "created_at" timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" timestamptz
 );
+
+ALTER TABLE "answer"
+ADD FOREIGN KEY ("question_id") REFERENCES "question" ("id");
+
+COMMIT;
